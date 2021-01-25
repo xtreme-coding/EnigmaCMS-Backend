1b89443b5b9fa1c89159761a9b172bcb8d0e83c5
Gourav Das <gourav2001das@gmail.com>
2021-01-25 21:24:55 +0530

    Resolved the issue of not working of backend server

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     # path('',views.user,name="user"),
-    path('register/', views.register, name='users-register'),
+    path('', views.register, name='users-register'),
     path('login/', views.login, name='users-login'),
 ]
